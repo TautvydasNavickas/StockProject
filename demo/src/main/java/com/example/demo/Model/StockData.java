2f517582710898a847ceb9e9f4a8238f9579ff31
tautv <taut.navickas@gmail.com>
2023-06-15 20:11:42 +0300

    Another model StockData was created

---
+++ demo/src/main/java/com/example/demo/Model/StockData.java
@@ -0,0 +1,130 @@
+package com.example.demo.Model;
+
+
+import java.time.LocalDate;
+
+public class StockData {
+    private LocalDate date;
+    private double open;
+    private double high;
+    private double low;
+    private double close;
+    private long volume;
+    private long unadjustedVolume;
+    private double adjClose;
+
+    private double change;
+    private double changePercent;
+    private double vwap;
+    private String label;
+    private double changeOverTime;
+
+    public double getChange() {
+        return change;
+    }
+
+    public void setChange(double change) {
+        this.change = change;
+    }
+
+    public double getChangePercent() {
+        return changePercent;
+    }
+
+    public void setChangePercent(double changePercent) {
+        this.changePercent = changePercent;
+    }
+
+    public double getVwap() {
+        return vwap;
+    }
+
+    public void setVwap(double vwap) {
+        this.vwap = vwap;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public double getChangeOverTime() {
+        return changeOverTime;
+    }
+
+    public void setChangeOverTime(double changeOverTime) {
+        this.changeOverTime = changeOverTime;
+    }
+
+    public long getUnadjustedVolume() {
+        return unadjustedVolume;
+    }
+
+    public void setUnadjustedVolume(long unadjustedVolume) {
+        this.unadjustedVolume = unadjustedVolume;
+    }
+
+    // Constructors, getters, and setters
+    public StockData() {
+    }
+
+    public double getAdjClose() {
+        return adjClose;
+    }
+
+    public void setAdjClose(double adjClose) {
+        this.adjClose = adjClose;
+    }
+// Getters and setters
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getOpen() {
+        return open;
+    }
+
+    public void setOpen(double open) {
+        this.open = open;
+    }
+
+    public double getHigh() {
+        return high;
+    }
+
+    public void setHigh(double high) {
+        this.high = high;
+    }
+
+    public double getLow() {
+        return low;
+    }
+
+    public void setLow(double low) {
+        this.low = low;
+    }
+
+    public double getClose() {
+        return close;
+    }
+
+    public void setClose(double close) {
+        this.close = close;
+    }
+
+    public long getVolume() {
+        return volume;
+    }
+
+    public void setVolume(long volume) {
+        this.volume = volume;
+    }
+}
--- demo/src/main/java/com/example/demo/StockProjectApplication.java
+++ demo/src/main/java/com/example/demo/StockProjectApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
 
 @SpringBootApplication
+
 public class StockProjectApplication {
 
 	public static void main(String[] args) {
